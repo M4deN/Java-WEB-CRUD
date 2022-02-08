83baf84a4762da8f71480fa86b8eba5dc5b3acd8
Alex L. Medeiros <Alexdesaran@outlook.com>
2022-02-08 09:54:48 -0300

    Update Conexao.java

--- src/java/DAO/Conexao.java
+++ src/java/DAO/Conexao.java
@@ -6,7 +6,7 @@ public class Conexao {
     public Connection getConexao(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost/BDAvaliacao?useSSL=false&useTimezone=true&serverTimezone=UTC","root","rebelderbd5640");
+            return DriverManager.getConnection("jdbc:mysql://localhost/BDAvaliacao?useSSL=false&useTimezone=true&serverTimezone=UTC","","");
         }catch(Exception erro){
             throw new RuntimeException("Erro classe conexão: "+erro);
         }
